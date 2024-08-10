76e44d9c502fdb8b19a3b0384447beec39c83f72
Gerardo Contijoch <gerardo.contijoch@gmail.com>
2024-08-10 15:21:34 -0300

    frontend: fix en la definicion del modelo usado como ejemplo de pruebas.

--- frontend/src/components/StockCard.jsx
+++ frontend/src/components/StockCard.jsx
@@ -1,5 +1,32 @@
 import styles from './StockCard.module.css';
 
+/**
+ * Formatea un numero como precio.
+ * @param {number} number
+ * @returns
+ */
+function formatPrice(number) {
+  if (Number.isNaN(number)) return 'n/a';
+
+  return `$${formatNumber(number)}`;
+}
+
+/**
+ * Formatea un numero como porcentaje.
+ * @param {number} number
+ * @returns
+ */
+function formatPerc(number) {
+  if (Number.isNaN(number)) return 'n/a';
+
+  return `${formatNumber(number)}%`;
+}
+
+/**
+ * Formatea un numero.
+ * @param {number} number
+ * @returns
+ */
 function formatNumber(number) {
   if (Number.isNaN(number)) return 'n/a';
 
@@ -16,7 +43,7 @@ function formatNumber(number) {
 
 /**
  *
- * @param {{symbol:string, price:number, marketCap: number, sharesOut:number, priceEarn:number, roa:number, roi:number, roe:number}} data
+ * @param {{symbol:string, price:number, marketCap: number, sharesOutsanding:number, priceEarnings:number, ROA:number, ROI:number, ROE:number}} data
  * @returns
  */
 export function StockCard({data}) {
@@ -26,31 +53,31 @@ export function StockCard({data}) {
       <ul>
         <li>
           <span className={styles.cardLabel}>Price:</span>
-          <span className={styles.cardValue}>${data.price.toFixed(2)}</span>
+          <span className={styles.cardValue}>{formatPrice(data.price.toFixed(2))}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>Market cap:</span>
-          <span className={styles.cardValue}>${formatNumber(data.marketCap)}</span>
+          <span className={styles.cardValue}>{formatPrice(data.marketCap)}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>Shares outstanding:</span>
-          <span className={styles.cardValue}>${formatNumber(data.sharesOut)}</span>
+          <span className={styles.cardValue}>{formatPrice(data.sharesOutsanding)}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>Price earnings:</span>
-          <span className={styles.cardValue}>${formatNumber(data.priceEarn)}</span>
+          <span className={styles.cardValue}>{formatPrice(data.priceEarnings)}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>ROA:</span>
-          <span className={styles.cardValue}>{formatNumber(data.roa / 100)}%</span>
+          <span className={styles.cardValue}>{formatPerc(data.ROA / 100)}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>ROI:</span>
-          <span className={styles.cardValue}>{formatNumber(data.roi / 100)}%</span>
+          <span className={styles.cardValue}>{formatPerc(data.ROI / 100)}</span>
         </li>
         <li>
           <span className={styles.cardLabel}>ROE:</span>
-          <span className={styles.cardValue}>{formatNumber(data.roe / 100)}%</span>
+          <span className={styles.cardValue}>{formatPerc(data.ROE / 100)}</span>
         </li>
       </ul>
     </div>
